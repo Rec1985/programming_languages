55d6b6f82885c96e7b8588d2aa48fed7553469bd
NIKA30 <rec11-07-1985@mail.ru>
2023-01-14 14:45:13 +0300

    done Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите натуральное число от 1 до 7");
+int numberWeek = Convert.ToInt32(Console.ReadLine());
+
+
+if(numberWeek == 1){
+    Console.Write("1 -> Понедельник");
+}else if (numberWeek == 2){
+    Console.Write("2 -> Вторник");
+}else if (numberWeek == 3){
+    Console.Write("3 -> Среда");
+}else if (numberWeek == 4){
+    Console.Write("4 -> Четверг");
+}else if (numberWeek == 5){
+    Console.Write("5 -> Пятница");
+}else if (numberWeek == 6){
+    Console.Write("6 -> Суббота");
+}else if (numberWeek == 7){
+    Console.Write("7 -> Воскресение");
+}else{
+    Console.Write($"{numberWeek} -> Вы ввели недопустимое значение!");
+}
+Console.WriteLine();
