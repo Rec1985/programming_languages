18e6ea57c3b41cbc671547e11cdfe63588aa6236
NIKA30 <rec11-07-1985@mail.ru>
2023-01-14 21:39:38 +0300

    done Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number%2 == 0){
+    Console.WriteLine("Да");
+}else{
+    Console.WriteLine("Нет");
+}
