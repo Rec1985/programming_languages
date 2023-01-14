0fb73fad63ca060d1c8c6cd0dbf8301888f0f7f2
NIKA30 <rec11-07-1985@mail.ru>
2023-01-14 14:18:14 +0300

    complit Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите первое натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number == number2 * number2)
+{
+    Console.WriteLine("Да");
+}else{
+    Console.WriteLine("Нет");
+}
