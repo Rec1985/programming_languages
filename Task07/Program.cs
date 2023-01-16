5a0c0f40dcfabba603f8ad7de01c311b09e97474
NIKA30 <rec11-07-1985@mail.ru>
2023-01-16 16:59:32 +0300

    done task07

---
+++ Task07/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("--------------------ПОСЛЕДНЯЯ ЦИФРА ТРЁХЗНАЧНОГО ЧИСЛА--------------------");
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number/100 > 0 && number/100 < 10)
+{
+    int result = number % 10;
+    Console.Write($"Последнее число: {result}");
+}else{
+    Console.WriteLine("Вы ввели не трёхзначное число!");
+}
