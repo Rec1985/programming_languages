ccbfbf19151901af3f918040cd49525176f5a16e
NIKA30 <rec11-07-1985@mail.ru>
2023-01-18 16:49:26 +0300

    done task11

---
+++ Task11/Program.cs
@@ -0,0 +1,22 @@
+﻿// Task11: 
+//     Напишите программу, которая выводит случайное
+//     трёхзначное число и удаляет вторую цифру этого
+//     числа.
+
+//     456 -> 46
+//     782 -> 72
+//     918 -> 98
+
+Console.WriteLine("--------------------УДАЛЯЕТ ИЗ ТРЕХЗНАЧНОГО ЧИСЛА ВТОРУЮ ЦИФРУ--------------------");
+
+int NumberConvert(int num)
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    int result = firstDigit * 10 + lastDigit;
+    return result;
+
+}
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трёзначное число: {number}");
+Console.WriteLine($"Итоговое двухначное число: {NumberConvert(number)}");
