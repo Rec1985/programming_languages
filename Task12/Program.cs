626b9e9fe741af630aa36a58ccbb70343e553cbb
NIKA30 <rec11-07-1985@mail.ru>
2023-01-18 17:09:38 +0300

    done task12

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("--------------------ПРИНИМАЕТ ДВА ЧИСЛА, ПРОВЕРЯЕТ ПЕРВОЕ КРАТНО ВТОРОМУ?--------------------");
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Multiplicity(num1, num2);
+
+void Multiplicity(int arg1, int arg2)
+{
+    if(arg1 % arg2 == 0 )   Console.WriteLine($"Число {arg1} кратно {arg2}");
+    else                    Console.WriteLine($"Число {arg1} не кратно {arg2} остаток = {arg1 % arg2}");
+}
