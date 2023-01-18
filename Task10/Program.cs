a1b561898529e3b37f9467deeafd945703610448
NIKA30 <rec11-07-1985@mail.ru>
2023-01-18 22:58:15 +0300

    done Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,36 @@
+﻿// Task10: 
+//     Напишите программу, которая 
+//     принимает на вход трёхзначное число и на 
+//     выходе показывает вторую цифру этого числа. 
+//     Выполнить с помощью числовых операций 
+//     (целочисленное деление, остаток от деления).
+
+//     456 -> 5
+//     782 -> 8
+//     918 -> 1
+
+Console.WriteLine("--------------------ПРИНИМАЕТ ТРЁХЗНАЧНОЕ ЧИСЛО, ВЫВОДИТ ВТОРУЮ ЦИФРУ--------------------");
+
+Console.Write("Введите число от 100 до 999: ");
+int number = Convert.ToInt32(Console.ReadLine());
+//int result = ConvertThreeToOne(number);
+
+if(ConvertThreeToOne(number) == -1)
+    Console.WriteLine("Введено не верное значение!");
+else Console.WriteLine(ConvertThreeToOne(number));
+
+
+int ConvertThreeToOne(int num)
+{
+    
+    if (100 <= num && num < 1000)
+    {
+        return num/10%10;
+    }
+    else
+        return -1;
+}
+
+
+
+
