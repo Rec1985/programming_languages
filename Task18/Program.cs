be102c1d86e0b7c9e34c95d337031221b0e05d9f
NIKA30 <rec11-07-1985@mail.ru>
2023-01-24 23:26:36 +0300

    done Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите номер четверти: ");
+string numQuarter = Console.ReadLine();
+string result = RangeCoordinates(numQuarter);
+Console.WriteLine(result);
+
+string RangeCoordinates(string cordinate)
+{
+    if(cordinate == "1") return ("x > 0, y > 0");
+    if(cordinate == "2") return ("x < 0, y > 0");
+    if(cordinate == "3") return ("x < 0, y < 0");
+    if(cordinate == "4") return ("x > 0, y < 0");
+    return ("Введите корректные данные!");
+};
---
+++ Task24/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Введите целое полоительое число: ");
+int number = Convert.ToInt32( Console.ReadLine());
+
+int sumNumbers = SumNumber(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers}");
+
+int SumNumber(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
