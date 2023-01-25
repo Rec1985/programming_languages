69fb242f005cdebd6f8463d186b8cadc41dc633c
NIKA30 <rec11-07-1985@mail.ru>
2023-01-25 14:57:05 +0300

    done Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+SquareTable(number);
+
+void SquareTable(int num)
+{
+    int count = 1;
+    while(count <= num)
+    {
+        Console.WriteLine($"{count} -> {count * count}");        
+        count++;
+    }
+}
+
