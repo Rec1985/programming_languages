222e399fc0d724a8c4ab30514f37e9e394ded1a9
NIKA30 <rec11-07-1985@mail.ru>
2023-01-26 19:30:19 +0300

    done Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.Write("Введите количество элементов в массиве: ");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение эллемента в массиве: ");
+int valueArr = Convert.ToInt32(Console.ReadLine());
+
+GenerateArray(sizeArr, valueArr);
+
+void GenerateArray(int size, int value)
+{
+    Random random = new Random();
+    int[] arrNumbers = new int [size];
+    for (int i = 0; i < arrNumbers.Length; i++)
+    {
+        arrNumbers[i] = random.Next(0, value);
+    }
+    PrintArray(arrNumbers);
+
+}
+
+void PrintArray(int[] inArr)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < inArr.Length; i++)
+    {
+        Console.Write($"{inArr[i]}");
+        if(i < inArr.Length-1) Console.Write($", ");
+    }
+    Console.Write(" ]");
+}
