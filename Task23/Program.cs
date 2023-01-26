e08511ee178f829270e23ea0df3c2385e436a04e
NIKA30 <rec11-07-1985@mail.ru>
2023-01-26 16:35:00 +0300

    done Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+CubeNumber(number);
+
+void CubeNumber(int num)
+{
+    int count = 1;
+    while(count <= num)
+    {
+        Console.WriteLine($"{count,5} -> {count * count * count,5}");        
+        count++;
+    }
+}
