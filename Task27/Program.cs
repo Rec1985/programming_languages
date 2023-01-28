075876791d47e6f56985ed419b9861b8f8eb9319
NIKA30 <rec11-07-1985@mail.ru>
2023-01-28 13:06:11 +0300

    done Tasks 25,27,29

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -3,4 +3,28 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SignSumOfCells(number);
+Console.Write($"{result}");
 
+int SignSumOfCells(int num) //Сумирует ячейки числа с учётом знака
+{
+    bool flag = true;
+    if(num < 0) //Если число отрицательное, преобразуем в положительное и ставим флажек
+    {
+        flag = false;
+        num *= -1;
+    }
+
+    int summ = num%10;
+    while (num > 0)
+    {
+        num = num / 10;
+        summ += num%10;
+    }
+
+    if(flag == false) summ *= -1;//Если стоял флажек, преобразуем результат назад в отрицательное
+
+    return summ;
+}
