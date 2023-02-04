431112c5dc4f9942465d14c4f34e827fafa525de
NIKA30 <rec11-07-1985@mail.ru>
2023-02-04 16:10:12 +0300

    done Task49

---
+++ Task49/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+int[,] eee = CreateMatrixArray(5, 5);
+PrintMatrix(eee);
+
+int[,] CreateMatrixArray(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i+j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4} | ");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
