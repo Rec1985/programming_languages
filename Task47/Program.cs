90630cb221ed193a48b1871a671bed2cd4d383c1
NIKA30 <rec11-07-1985@mail.ru>
2023-02-13 11:45:31 +0300

    done Task47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -33,7 +33,7 @@ void PrintMatrix(double[,] matrix)
         Console.Write("|");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");// 4--- это форматирование для вывода массива
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4} ");
             else Console.Write($"{matrix[i, j],4} ");
         }
         Console.WriteLine("|");
