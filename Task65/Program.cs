884467e2cc74a070d85e45a5f707f1e357dfe8be
Vladimir Mironov <rec11-07-1985@mail.ru>
2023-02-14 18:08:10 +0300

    done Task65

---
+++ Task65/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите натуральное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(numberN, numberM);
+
+void NaturalNumbers(int num1, int num2)
+{
+    if(num1 < num2)
+    {
+        Console.Write($"{num1} ");
+        NaturalNumbers(num1 + 1, num2);
+    }
+    else if (num1 > num2)
+    {
+        Console.Write($"{num1} ");
+        NaturalNumbers(num1 - 1, num2);
+    }
+    else Console.WriteLine(num2);
+}
