ea5a97d2a7d68ee32908666521e5795330511de1
Vladimir Mironov <rec11-07-1985@mail.ru>
2023-02-14 17:46:00 +0300

    done Task63

--- Task63/Program.cs
+++ Task63/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number);
+
+void NaturalNumbers(int num)
+{
+    if(num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");   
+}
