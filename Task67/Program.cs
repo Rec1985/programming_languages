803af69d5089c38200bb83af2fde3354d949fc63
Vladimir Mironov <rec11-07-1985@mail.ru>
2023-02-15 12:54:38 +0300

    done Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int  summNumbers = SummNumbers(num);
+Console.WriteLine($"Сумма цифр числа равна: {summNumbers}");
+
+int SummNumbers(int number)
+{
+    if(number == 0) return 0;
+    else return number % 10 + SummNumbers(number /10);
+}
