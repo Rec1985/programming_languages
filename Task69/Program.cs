f76346b3894c7548e4557fdcd8549a877b46879b
Vladimir Mironov <rec11-07-1985@mail.ru>
2023-02-15 11:18:12 +0300

    done Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int power = PowerNumbers(numberA, numberB);
+Console.WriteLine($"A в степени B: {power}");
+
+int PowerNumbers(int num1, int num2)
+{
+    if(num2 == 0) return 1;
+    else return num1 * PowerNumbers(num1, num2 -1);    
+}
